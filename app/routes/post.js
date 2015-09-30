eda240dc9a6f6e7fd55452c23c4ea3f6c46b9a8d
Molly Waggett & yvonne peng <student@epicodus.com>
2015-09-30 10:38:07 -0700

    Allow user to delete a post.

--- app/routes/post.js
+++ app/routes/post.js
@@ -3,5 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
+  },
+  actions: {
+    updatePost(post, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          post.set(key, params[key]);
+        }
+      });
+      post.save();
+    },
+
+    deletePost(post) {
+      post.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
